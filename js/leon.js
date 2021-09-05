9d2cb12e3e92d60fa48b89260646c6c4aac72c2c
andrey1303110 <andrey1303110@gmail.com>
2021-09-05 17:06:32 +0300

    test

--- js/leon.js
+++ js/leon.js
@@ -40,6 +40,67 @@ var direction = "",
         $('#slick-slide02')[0].src = `./images/Cars_main_screen/all_cars/leon/white/${iLeon}.png`;
     }
 
-$('#leon_8').on('mousemove', mousemovemethod);
-$('#leon_7').on('mousemove', mousemovemethod);
-$('#leon_1').on('mousemove', mousemovemethod);
+    if (window.innerWidth > 550) {
+        $('#leon_8').on('mousemove', mousemovemethod);
+        $('#leon_7').on('mousemove', mousemovemethod);
+        $('#leon_1').on('mousemove', mousemovemethod);
+    }
+
+    if (window.innerWidth < 550) {
+        if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
+  
+            let lastReadingTimestamp;
+            let accelerometer = new LinearAccelerationSensor();
+            accelerometer.addEventListener('reading', e => {
+              if (lastReadingTimestamp) {
+                intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
+              }
+              lastReadingTimestamp = accelerometer.timestamp
+              accelerationHandler(accelerometer, 'moAccel');
+            });
+            accelerometer.start();
+            
+            if ('GravitySensor' in window) {
+              let gravity = new GravitySensor();
+              gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
+              gravity.start();
+            }
+            
+            let gyroscope = new Gyroscope();
+            gyroscope.addEventListener('reading', e => rotationHandler({
+              alpha: gyroscope.x,
+              beta: gyroscope.y,
+              gamma: gyroscope.z
+            }));
+            gyroscope.start();
+            
+        } else if ('DeviceMotionEvent' in window) {
+        
+        var onDeviceMotion = function (eventData) {
+            accelerationHandler(eventData.acceleration, 'moAccel');
+            accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
+            rotationHandler(eventData.rotationRate);
+            intervalHandler(eventData.interval);
+        }
+        
+        window.addEventListener('devicemotion', onDeviceMotion, false);
+        }
+        
+        function rotationHandler(rotation) {
+            if (rotation.beta > 1.5) {
+                iLeon += 1;
+                if (iLeon >= 25) {
+                    iLeon = 1;
+                }
+            }
+            if (rotation.beta < -1.5) {
+                iLeon -= 1;
+                if (iLeon < 1) {
+                    iLeon = 25;
+                }
+            }
+            $('#slick-slide00')[0].src = `./images/Cars_main_screen/all_cars/leon/blue/${iLeon}.png`;
+            $('#slick-slide01')[0].src = `./images/Cars_main_screen/all_cars/leon/red/${iLeon}.png`;
+            $('#slick-slide02')[0].src = `./images/Cars_main_screen/all_cars/leon/white/${iLeon}.png`;
+        }
+    }
